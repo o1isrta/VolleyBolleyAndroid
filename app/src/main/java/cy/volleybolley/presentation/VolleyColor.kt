dafe31c5f64ea5f385539457b8648b896e1f539a
Svetlana <s.a.novakovskaya@yandex.ru>
2025-07-04 01:15:31 +0500

    Добавлен файл цветов

---
+++ app/src/main/java/cy/volleybolley/presentation/VolleyColor.kt
@@ -0,0 +1,19 @@
+package cy.volleybolley.presentation
+
+import androidx.compose.ui.graphics.Color
+
+class VolleyColor {
+    val OrangeHard = Color(0xFFE6AC53)          // оранжевый (стрелки, звездочки, кнопка HARD)
+    val YellowPro = Color(0xFFE6C953)           // желтый (кнопки, в том числе кнопка PRO)
+    val YellowForGradient = Color(0xFFF4E998)   // желтый для градиента на кнопках
+    val GreenForGradient = Color(0xFF5CF08D)    // зеленый для градиента на кнопках
+    val TurquoiseMedium = Color(0xFF2DB69A)     // бирюзовый (кнопка MEDIUM)
+    val Turquoise = Color(0xFF53A8A1)           // бирюзовый (светлый фон)
+    val TurquoiseDark = Color(0xFF32716B)       // темный бирюзовый (темный фон)
+    val TurquoiseBottom = Color(0xFF295E6D)     // самый темный бирюзовый (для BottomNavigationBar)
+    val BlueLight = Color(0xFF53A4E6)           // голубой (кнопка LIGHT)
+    val GreyDark = Color(0xFF516372)            // темный серый
+    val GreyDisabled = Color(0xFF8CA5A3)        // серый (для неактивных кнопок)
+    val White = Color(0xFFFFFFFF)               // белый
+    val BlackText = Color(0xFF423F39)           // цвет темного текста
+}
