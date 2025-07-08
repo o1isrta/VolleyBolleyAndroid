8ed49bd9ad54b55d9013809a0c668caff656efff
nemislimus <nemislimus@yandex.ru>
2025-07-08 21:32:44 +0300

    #21 Добавил поле с счётчиком

--- app/src/main/java/cy/volleybolley/core/presentation/ui/VolleyTextField.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/VolleyTextField.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,6 +27,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -176,6 +178,7 @@ object VolleyTextField {
         cornerRadius: Int = VolleyDimens.DIMEN_16,
         height: Int = VolleyDimens.DIMEN_52,
         hint: String = stringResource(R.string.registration_date_of_birth_hint),
+        actionForSaveDate: (String) -> Unit,
     ) {
         val shape = RoundedCornerShape(cornerRadius.dp)
         var showModal by remember { mutableStateOf(false) }
@@ -205,11 +208,11 @@ object VolleyTextField {
                         shape = shape
                     )
                     .border(
-                        width = 1.dp,
+                        width = VolleyDimens.DIMEN_1.dp,
                         brush = gradientBrush,
                         shape = shape
                     )
-                    .padding(22.dp, 0.dp)
+                    .padding(VolleyDimens.DIMEN_22.dp, 0.dp)
             ) {
                 Text(
                     text = correctText,
@@ -224,7 +227,8 @@ object VolleyTextField {
                 onDateSelected = { date ->
                     selectedDate = date
                 },
-                onDismiss = { showModal = false }
+                onDismiss = { showModal = false },
+                actionForSaveDate = actionForSaveDate,
             )
         }
     }
@@ -233,7 +237,8 @@ object VolleyTextField {
     @Composable
     private fun DatePickerModal(
         onDateSelected: (Long?) -> Unit,
-        onDismiss: () -> Unit
+        onDismiss: () -> Unit,
+        actionForSaveDate: (String) -> Unit
     ) {
         val datePickerState = rememberDatePickerState()
 
@@ -243,6 +248,8 @@ object VolleyTextField {
             confirmButton = {
                 TextButton(onClick = {
                     onDateSelected(datePickerState.selectedDateMillis)
+                    val dateString = datePickerState.selectedDateMillis?.let { convertMillisToDate(it) } ?: ""
+                    actionForSaveDate(dateString)
                     onDismiss()
                 }) {
                     Text(
@@ -303,13 +310,103 @@ object VolleyTextField {
         return formatter.format(Date(millis))
     }
 
+    @Composable
+    fun CountActionField(
+        maximumCount: Int = VolleyDimens.DIMEN_24,
+        minimumCount: Int = VolleyDimens.DIMEN_4,
+        paddingValues: PaddingValues = PaddingValues(),
+        actionForSaveCount: (Int) -> Unit,
+    ) {
+
+        var count by remember { mutableIntStateOf(minimumCount) }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (count > minimumCount) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_minus),
+                        contentDescription = null,
+                        tint = VolleyColor.WHITE,
+                        modifier = Modifier
+                            .clickable(null,null) {
+                                count -= 1
+                                actionForSaveCount(count)
+                            }
+                    )
+                    Spacer(Modifier.width(VolleyDimens.DIMEN_8.dp))
+                }
+
+                CountField(text = count.toString())
+
+                if (count < maximumCount) {
+                    Spacer(Modifier.width(VolleyDimens.DIMEN_8.dp))
+                    Icon(
+                        painter = painterResource(R.drawable.ic_plus),
+                        contentDescription = null,
+                        tint = VolleyColor.WHITE,
+                        modifier = Modifier
+                            .clickable(null,null) {
+                                count +=1
+                                actionForSaveCount(count)
+                            }
+                    )
+                }
+            }
+        }
+
+    }
+
+    @Composable
+    private fun CountField(
+        modifier: Modifier = Modifier,
+        cornerRadius: Int = VolleyDimens.DIMEN_16,
+        height: Int = VolleyDimens.DIMEN_40,
+        text: String,
+    ) {
+        val shape = RoundedCornerShape(cornerRadius.dp)
+
+        val gradientBrush = Brush.verticalGradient(
+            colors = listOf(VolleyColor.YELLOW_GRADIENT, VolleyColor.GREEN_GRADIENT)
+        )
+
+        Box(
+            modifier = modifier
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .height(height.dp)
+                    .background(
+                        color = Color.White,
+                        shape = shape
+                    )
+                    .border(
+                        width = VolleyDimens.DIMEN_1.dp,
+                        brush = gradientBrush,
+                        shape = shape
+                    )
+                    .padding(VolleyDimens.DIMEN_27.dp, 0.dp)
+            ) {
+                Text(
+                    text = text,
+                    color = VolleyColor.TEXT_FIELD,
+                    style = TextStyleGradientField,
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun PreviewGradientTextFields() {
     Root {
-        Column {
+        Column(modifier = Modifier.background(VolleyColor.TEXT_CALENDAR_LIGHT_GREY)) {
 
             Spacer(modifier = Modifier.height(VolleyDimens.DIMEN_44.dp))
 
@@ -327,7 +424,11 @@ private fun PreviewGradientTextFields() {
 
             VolleyTextField.DataPickerField(
                 modifier = Modifier.padding(VolleyDimens.DIMEN_16.dp)
-            )
+            ) { }
+
+            VolleyTextField.CountActionField(
+                paddingValues = PaddingValues(VolleyDimens.DIMEN_16.dp)
+            ) { }
 
         }
     }
