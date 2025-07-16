6de2cfb35517a7bff44bf0bb85e2fc00afb57ee6
Pavel_Davydov <david20506@yandex.ru>
2025-07-16 21:49:33 +0500

    Вернул MainActivity.kt

--- app/src/main/java/cy/volleybolley/MainActivity.kt
+++ app/src/main/java/cy/volleybolley/MainActivity.kt
@@ -1,16 +1,16 @@
 package cy.volleybolley
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import cy.volleybolley.auth.ui.AuthScreen
 import cy.volleybolley.ui.theme.VolleybolleyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VolleybolleyTheme {
-                val clientId = stringResource(R.string.default_web_client_id)
-                AuthScreen(clientId) { token ->
-                    Log.d("ГуглТокен", token)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VolleybolleyTheme {
+        Greeting("Android")
     }
 }
