d4f8620f284c225b8924ab7c67f52d4b24b310be
A2T-user <luidor-arb@yandex.ru>
2025-07-25 15:19:50 +0400

    Устранение замечаний detekt

--- app/src/main/java/cy/volleybolley/core/presentation/ui/model/Colors.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/model/Colors.kt
@@ -3,6 +3,11 @@ package cy.volleybolley.core.presentation.ui.model
 import androidx.compose.ui.graphics.Color
 
 object Colors {
-    val White = Color(0xFFFFFFFF)
-    val ColorHeader = Color(0xFF53A8A1)
+    val White = Color(AppColors.WHITE)
+    val ColorHeader = Color(AppColors.COLOR_HEDER)
+}
+
+object AppColors {
+    const val WHITE = 0xFFFFFFFF
+    const val COLOR_HEDER = 0xFF53A8A1
 }
--- app/src/main/java/cy/volleybolley/ui/theme/Color.kt
+++ app/src/main/java/cy/volleybolley/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package cy.volleybolley.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 val Purple80 = Color(color = DefaultThemeColors.PURPLE_80)
 val PurpleGrey80 = Color(color = DefaultThemeColors.PURPLE_GREY_80)
 val Pink80 = Color(color = DefaultThemeColors.PINK_80)
@@ -10,7 +9,6 @@ val Pink80 = Color(color = DefaultThemeColors.PINK_80)
 val Purple40 = Color(color = DefaultThemeColors.PURPLE_40)
 val PurpleGrey40 = Color(color = DefaultThemeColors.PURPLE_GREY40)
 val Pink40 = Color(color = DefaultThemeColors.PINK_40)
-
 
 object DefaultThemeColors {
     const val PURPLE_80 = 0xFFD0BCFF
@@ -20,4 +18,3 @@ object DefaultThemeColors {
     const val PURPLE_GREY40 = 0xFFD0BCFF
     const val PINK_40 = 0xFFD0BCFF
 }
-
