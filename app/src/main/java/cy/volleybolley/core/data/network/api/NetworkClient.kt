9a528840592a1d8b723b846ca02d8d2ed47de96b
nemislimus <nemislimus@yandex.ru>
2025-07-25 10:25:04 +0300

    #42 detekt_fix

--- app/src/main/java/cy/volleybolley/core/data/network/api/NetworkClient.kt
+++ app/src/main/java/cy/volleybolley/core/data/network/api/NetworkClient.kt
@@ -2,7 +2,7 @@ package cy.volleybolley.core.data.network.api
 
 import cy.volleybolley.core.data.network.model.Response
 
-interface NetworkClient<T,R> {
+interface NetworkClient<T, R> {
     suspend fun getResponse(sealedRequest: T): Response<R>
 
     companion object {
