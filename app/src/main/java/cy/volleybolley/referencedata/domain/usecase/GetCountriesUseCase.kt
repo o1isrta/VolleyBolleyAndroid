219e91348c11cabb95c52dc7b7a5ee9e0045fee6
Keithcutt <mayerhofnew@gmail.com>
2025-07-27 23:34:08 +0300

    Domain-слой. Созданы интерактор и репозиторий для работы со справочниками. Созданы модели.

---
+++ app/src/main/java/cy/volleybolley/referencedata/domain/usecase/GetCountriesUseCase.kt
@@ -0,0 +1,10 @@
+package cy.volleybolley.referencedata.domain.usecase
+
+import cy.volleybolley.core.domain.model.ErrorType
+import cy.volleybolley.core.domain.model.VolleyResult
+import cy.volleybolley.referencedata.domain.model.Country
+import kotlinx.coroutines.flow.Flow
+
+interface GetCountriesUseCase {
+    fun execute(): Flow<VolleyResult<List<Country>, ErrorType>>
+}
