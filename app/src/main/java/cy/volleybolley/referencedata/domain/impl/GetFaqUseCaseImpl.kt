858abfa1cd7bc5e5259e270830a6ed7bfaed775b
Keithcutt <mayerhofnew@gmail.com>
2025-07-28 23:20:52 +0300

    Добавлены недостающие имплементации usecase'ов.

---
+++ app/src/main/java/cy/volleybolley/referencedata/domain/impl/GetFaqUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package cy.volleybolley.referencedata.domain.impl
+
+import cy.volleybolley.core.domain.model.ErrorType
+import cy.volleybolley.core.domain.model.VolleyResult
+import cy.volleybolley.referencedata.domain.api.ReferenceDataRemoteRepository
+import cy.volleybolley.referencedata.domain.model.Faq
+import cy.volleybolley.referencedata.domain.usecase.GetFaqUseCase
+import kotlinx.coroutines.flow.Flow
+
+class GetFaqUseCaseImpl(private val referenceDataRemoteRepository: ReferenceDataRemoteRepository) : GetFaqUseCase {
+    override fun execute(): Flow<VolleyResult<Faq, ErrorType>> {
+        return referenceDataRemoteRepository.getFaq()
+    }
+}
