66c4bd9c3357c0e47c95bf6ee89e73e429666576
Keithcutt <mayerhofnew@gmail.com>
2025-07-28 23:31:30 +0300

    Добавлен di-модуль

--- app/src/main/java/cy/volleybolley/core/DiProvider.kt
+++ app/src/main/java/cy/volleybolley/core/DiProvider.kt
@@ -8,6 +8,8 @@ import cy.volleybolley.courts.di.courtsModule
 object DiProvider {
     val modules = listOf(
         coreModule,
+        referenceDataModule
+        coreModule,
         courtsModule,
         authViewModelModule,
         referenceDataModule,
