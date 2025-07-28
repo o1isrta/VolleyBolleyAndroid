d955ef931a8c87db63f3a0cb8b15b5d321217964
Keithcutt <mayerhofnew@gmail.com>
2025-07-28 21:44:22 +0300

    Data-слой. Созданы репозиторий и NetworkClient. Созданы dto-модели, модели запросов и ответов.

--- app/src/main/java/cy/volleybolley/referencedata/data/dto/CountryDto.kt
+++ app/src/main/java/cy/volleybolley/referencedata/data/dto/CountryDto.kt
@@ -12,15 +12,3 @@ data class CountryDto(
     val name: String? = null,
     val cities: List<CityDto>? = null
 )
-
-fun CountryDto.mapToDomain(): Country {
-    return Country(
-        id = id,
-        name = name,
-        cities = cities?.map { cityDto -> cityDto.mapToDomain() }
-    )
-}
-
-fun List<CountryDto>.mapToDomain(): List<Country> {
-    return this.map { it.mapToDomain() }
-}
