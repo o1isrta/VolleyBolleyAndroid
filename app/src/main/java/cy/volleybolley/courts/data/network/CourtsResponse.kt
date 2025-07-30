9c9e5c6d32ffedac512377e194c1dab543cd8851
ShevaVarya <shevchik.varvara@mail.ru>
2025-07-30 19:03:00 +0300

    37 - убран лишний @Serializable

--- app/src/main/java/cy/volleybolley/courts/data/network/CourtsResponse.kt
+++ app/src/main/java/cy/volleybolley/courts/data/network/CourtsResponse.kt
@@ -1,10 +1,8 @@
 package cy.volleybolley.courts.data.network
 
 import cy.volleybolley.courts.data.dto.CourtDto
-import kotlinx.serialization.Serializable
 
 sealed interface CourtsResponse {
-    @Serializable
     class GetCourts(
         val courts: List<CourtDto>
     ) : CourtsResponse
