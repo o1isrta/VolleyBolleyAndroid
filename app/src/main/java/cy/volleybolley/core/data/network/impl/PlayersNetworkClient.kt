2349adf8510a941450d0877b8754d3a46032c470
Dmitrii <richard.orlov13@gmail.com>
2025-08-07 22:33:11 +0300

    detekt fix 2

--- app/src/main/java/cy/volleybolley/core/data/network/impl/PlayersNetworkClient.kt
+++ app/src/main/java/cy/volleybolley/core/data/network/impl/PlayersNetworkClient.kt
@@ -3,12 +3,12 @@ package cy.volleybolley.core.data.network.impl
 import cy.volleybolley.core.data.network.model.PlayerDto
 import cy.volleybolley.core.data.network.model.PlayerRequest
 import cy.volleybolley.core.data.network.model.PlayerResponse
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
-import io.ktor.client.request.delete
-import io.ktor.http.path
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.path
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 import org.koin.core.component.inject
@@ -25,6 +25,7 @@ class PlayersNetworkClient : KtorNetworkClient<PlayerRequest, PlayerResponse>() 
             is PlayerRequest.GetAllPlayers -> {
                 httpClient.get { url { path(PLAYERS_PATH) } }
             }
+
             is PlayerRequest.SearchPlayers -> {
                 httpClient.get {
                     url {
@@ -33,12 +34,15 @@ class PlayersNetworkClient : KtorNetworkClient<PlayerRequest, PlayerResponse>() 
                     }
                 }
             }
+
             is PlayerRequest.GetPlayerDetail -> {
                 httpClient.get { url { path(PLAYERS_PATH, request.playerId.toString()) } }
             }
+
             is PlayerRequest.AddToFavorites -> {
                 httpClient.post { url { path(PLAYERS_PATH, request.playerId.toString(), FAVORITE_PATH) } }
             }
+
             is PlayerRequest.RemoveFromFavorites -> {
                 httpClient.delete { url { path(PLAYERS_PATH, request.playerId.toString(), FAVORITE_PATH) } }
             }
@@ -57,10 +61,12 @@ class PlayersNetworkClient : KtorNetworkClient<PlayerRequest, PlayerResponse>() 
                 val list = json.decodeFromString(ListSerializer(PlayerDto.serializer()), bodyText)
                 PlayerResponse.PlayerList(list)
             }
+
             is PlayerRequest.GetPlayerDetail -> {
                 val detail = json.decodeFromString(PlayerResponse.PlayerDetail.serializer(), bodyText)
                 detail
             }
+
             is PlayerRequest.AddToFavorites,
             is PlayerRequest.RemoveFromFavorites -> {
                 if (bodyText.isNotBlank()) {
