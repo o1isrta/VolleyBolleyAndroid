851cb174449b433e7e97345d24832727a59c0962
Cdarius <den-bykov@yandex-team.ru>
2025-08-07 22:22:00 +0300

    #34 Fix build

--- ci/add_google_services_file.sh
+++ ci/add_google_services_file.sh
@@ -1,3 +1,3 @@
 properties="${VOLLEY_GOOGLE_SERVICES}"
 
-touch google-services.json | echo "$properties" >> google-services.json
+touch app/google-services.json | echo "$properties" >> app/google-services.json
