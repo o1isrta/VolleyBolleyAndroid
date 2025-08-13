b1659897da3ec5ec4261f4d7124295582e5cc55e
svetSanna <s.a.novakovskaya@yandex.ru>
2025-08-13 23:29:45 +0300

    1 убрала isExclusive, CheckSet и state (то есть логику) из GroupButtonsForTourneyType 2 добавила кнопки-слайдеры 3 попробовала ActiveGradeintButton сделать не через box, а через button. Надо отпределиться, как делать

--- app/src/main/java/cy/volleybolley/core/presentation/ui/screens/createnewgame/BasicGameSetupScreen.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/screens/createnewgame/BasicGameSetupScreen.kt
@@ -1,13 +1,26 @@
 package cy.volleybolley.core.presentation.ui.screens.createnewgame
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import cy.volleybolley.core.presentation.ui.component.VolleyTopBar.TopBar
 
 @Composable
 fun BasicGameSetupScreen(navController: NavHostController) {
     Button(onClick = { navController.popBackStack() }) {
         Text("Назад")
+    }
+}
+
+@Preview
+@Composable
+private fun GameSetupScreenPreview() {
+    val navController = rememberNavController() // Создаем моковый NavHostController
+    Box {
+        BasicGameSetupScreen(navController = navController)
     }
 }
