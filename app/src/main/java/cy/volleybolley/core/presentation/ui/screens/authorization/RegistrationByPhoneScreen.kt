a286a87777c91f14007b0e94a696f1abfc754e24
Pavel_Davydov <david20506@yandex.ru>
2025-08-16 18:39:58 +0500

    Пробная верстка

--- app/src/main/java/cy/volleybolley/core/presentation/ui/screens/authorization/RegistrationByPhoneScreen.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/screens/authorization/RegistrationByPhoneScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import cy.volleybolley.core.presentation.ui.navigation.RegistrationRoute
@@ -25,6 +26,24 @@ fun RegistrationByPhoneScreen(navController: NavHostController) {
         Spacer(Modifier.height(8.dp))
         Button(onClick = {
             navController.navigate(RegistrationRoute)
+        }) {
+            Text("Go to Registration Screen")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegistrationByPhoneScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(8.dp))
+        Button(onClick = {
+
         }) {
             Text("Go to Registration Screen")
         }
