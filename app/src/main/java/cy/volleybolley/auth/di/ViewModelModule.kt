f736019d5abba5c333d02b8bbd831fe824a937ea
Pavel_Davydov <david20506@yandex.ru>
2025-08-18 16:40:22 +0500

    DI

--- app/src/main/java/cy/volleybolley/auth/di/ViewModelModule.kt
+++ app/src/main/java/cy/volleybolley/auth/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package cy.volleybolley.auth.di
 import com.google.firebase.auth.FirebaseAuth
 import cy.volleybolley.auth.ui.PhoneAuthHelper
 import cy.volleybolley.auth.ui.presentation.AuthViewModel
+import cy.volleybolley.auth.ui.presentation.PhoneAuthDelegate
 import cy.volleybolley.auth.ui.presentation.PhoneAuthViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -10,7 +11,7 @@ import org.koin.dsl.module
 val authViewModelModule = module {
     single { FirebaseAuth.getInstance() }
     single { PhoneAuthHelper(get()) }
-    single { PhoneAuthUiDelegate(get()) }
+    single { PhoneAuthDelegate(get()) }
     viewModel {
         AuthViewModel()
     }
