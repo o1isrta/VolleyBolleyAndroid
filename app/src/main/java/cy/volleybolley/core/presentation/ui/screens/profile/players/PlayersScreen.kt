ffc6906de45f4d927339448d6d1ede1995999171
nemislimus <nemislimus@yandex.ru>
2025-09-01 22:57:54 +0300

    #60 PlayersScreen: create layout tryout_2 (switch mode)

--- app/src/main/java/cy/volleybolley/core/presentation/ui/screens/profile/players/PlayersScreen.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/screens/profile/players/PlayersScreen.kt
@@ -1,30 +1,42 @@
 package cy.volleybolley.core.presentation.ui.screens.profile.players
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.google.firebase.annotations.concurrent.Background
 import cy.volleybolley.R
 import cy.volleybolley.core.presentation.ui.VolleyContainersRootTransparent
 import cy.volleybolley.core.presentation.ui.VolleySimpleComponent
 import cy.volleybolley.core.presentation.ui.VolleyTextFieldGradient
 import cy.volleybolley.core.presentation.ui.model.VolleyColor
 import cy.volleybolley.core.presentation.ui.model.VolleyDimens
+import cy.volleybolley.core.presentation.ui.model.VolleyText
 import cy.volleybolley.core.presentation.ui.navigation.NavMap
 import cy.volleybolley.core.presentation.ui.screens.profile.players.PlayersScreenEffect.NavigateFromPlayersScreen
 import cy.volleybolley.core.presentation.ui.screens.profile.players.PlayersScreenEvent.ClickOnBackFromPlayers
@@ -86,6 +98,11 @@ private fun PlayersScreen(
             )
 
             Spacer(Modifier.height(VolleyDimens.DIMEN_16.dp))
+
+            PlayersListModeSwitch(
+                onAllClick = {},
+                onFavoriteClick = {},
+            )
         }
     }
 
@@ -94,6 +111,83 @@ private fun PlayersScreen(
             is NavigateFromPlayersScreen -> navigateAction(effect.route)
             null -> {}
         }
+    }
+}
+
+@Composable
+private fun PlayersListModeSwitch(
+    paddingValues: PaddingValues = PaddingValues(),
+    height: Int = VolleyDimens.DIMEN_32,
+    cornerRadius: Int = VolleyDimens.DIMEN_16,
+    showAllPlayers: Boolean = true,
+    onAllClick: () -> Unit,
+    onFavoriteClick: () -> Unit,
+) {
+    val gradientBrush = remember {
+        Brush.verticalGradient(
+            colors = listOf(
+                VolleyColor.YellowForGradient,
+                VolleyColor.GreenForGradient
+            )
+        )
+    }
+
+    val shape = RoundedCornerShape(cornerRadius.dp)
+
+    Box(
+        modifier = Modifier.padding(paddingValues)
+            .height(height.dp)
+            .fillMaxWidth()
+            .background(gradientBrush, shape)
+    ) {
+        Row(Modifier.padding(VolleyDimens.DIMEN_2.dp)) {
+            ChangedBackgroundBox(
+                text = stringResource(R.string.all_players),
+                shape = shape,
+                showBackground = showAllPlayers,
+                onTextClick = onAllClick,
+                modifier = Modifier.weight(1f)
+            )
+
+            Spacer(Modifier.width(VolleyDimens.DIMEN_8.dp))
+
+            ChangedBackgroundBox(
+                text = stringResource(R.string.favorites),
+                shape = shape,
+                showBackground = !showAllPlayers,
+                onTextClick = onFavoriteClick,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ChangedBackgroundBox(
+    modifier: Modifier = Modifier,
+    text: String,
+    shape: Shape,
+    showBackground: Boolean,
+    onTextClick: () -> Unit,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxHeight()
+            .background(
+                color = if (showBackground) VolleyColor.White else Color.Transparent,
+                shape = shape
+            )
+    ) {
+        VolleyText.BodyRegular(
+            text = text,
+            color = VolleyColor.TextDark,
+            maxLines = 1,
+            modifier = Modifier.clickable(
+                interactionSource = null,
+                indication = null,
+                onClick = onTextClick
+            )
+        )
     }
 }
 
@@ -107,7 +201,12 @@ private fun PreviewPlayersScreen() {
                 .fillMaxSize()
                 .background(VolleyColor.TurquoiseDark)
         ) {
-            //////
+            PlayersScreen(
+                state = PlayersScreenState(),
+                effect = null,
+                navigateAction = {},
+                eventCallback = {}
+            )
         }
     }
 }
