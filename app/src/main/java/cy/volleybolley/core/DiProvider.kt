4e8e80363d8922408084de804bb423b6cbca99b4
ShevaVarya <shevchik.varvara@mail.ru>
2025-09-10 17:00:13 +0300

    47 - добавление в di profileModule

--- app/src/main/java/cy/volleybolley/core/DiProvider.kt
+++ app/src/main/java/cy/volleybolley/core/DiProvider.kt
@@ -4,12 +4,14 @@ import cy.volleybolley.auth.di.authViewModelModule
 import cy.volleybolley.core.di.coreModule
 import cy.volleybolley.courts.di.courtsModule
 import cy.volleybolley.games.di.gamesModule
+import cy.volleybolley.profile.di.profileModule
 
 object DiProvider {
     val modules = listOf(
         coreModule,
         courtsModule,
         gamesModule,
+        profileModule,
         authViewModelModule
     )
 }
