eb2d73eb9caf851e92fc516c72dd0d3351eb7158
nemislimus <nemislimus@yandex.ru>
2025-09-13 15:34:20 +0300

    #75 add deps, res, models

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
