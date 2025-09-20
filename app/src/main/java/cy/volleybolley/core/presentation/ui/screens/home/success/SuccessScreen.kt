13a1d29de48a91d1ce9e3ebedd0ba72fb4bd1b00
ShevaVarya <shevchik.varvara@mail.ru>
2025-09-20 11:26:59 +0300

    77 - мердж дев и ветки с кнопками

--- app/src/main/java/cy/volleybolley/core/presentation/ui/screens/home/success/SuccessScreen.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/screens/home/success/SuccessScreen.kt
@@ -8,23 +8,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,6 +27,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import cy.volleybolley.R
 import cy.volleybolley.core.presentation.ui.VolleyContainersRootTransparent.TransparentContainer
+import cy.volleybolley.core.presentation.ui.component.VolleyButton
 import cy.volleybolley.core.presentation.ui.component.VolleyButton.ActiveButton
 import cy.volleybolley.core.presentation.ui.model.VolleyColor
 import cy.volleybolley.core.presentation.ui.model.VolleyDimens
@@ -175,19 +169,16 @@ private fun SuccessScreen(
             }
 
             Row(
-                Modifier.padding(
-                    start = VolleyDimens.DIMEN_8.dp,
-                    end = VolleyDimens.DIMEN_8.dp,
-                    top = VolleyDimens.DIMEN_8.dp
-                ),
                 horizontalArrangement = Arrangement.spacedBy(VolleyDimens.DIMEN_8.dp)
             ) {
-                InvitePlayersButton(Modifier.weight(1f)) {
-                    eventCallback(SuccessEvent.OnInvitePlayers)
-                }
-                ShareButton(Modifier.weight(1f)) {
-                    eventCallback(SuccessEvent.OnShareLink)
-                }
+                VolleyButton.GroupInvitesButtons(
+                    onInvitePlayersClick = {
+                        eventCallback(SuccessEvent.OnInvitePlayers)
+                    },
+                    onShareLinkClick = {
+                        eventCallback(SuccessEvent.OnShareLink)
+                    }
+                )
             }
         }
     }
@@ -229,86 +220,13 @@ private fun RowIconText(
     }
 }
 
-@Composable
-private fun InvitePlayersButton(
-    modifier: Modifier,
-    onClick: () -> Unit
-) {
-    Button(
-        modifier = modifier.height(VolleyDimens.DIMEN_180.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = VolleyColor.YellowPro,
-        ),
-        contentPadding = PaddingValues(
-            top = VolleyDimens.DIMEN_20.dp,
-            start = VolleyDimens.DIMEN_20.dp
-        ),
-        shape = RoundedCornerShape(VolleyDimens.DIMEN_32.dp),
-        onClick = onClick
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Image(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd),
-                painter = painterResource(R.drawable.image_invite_players),
-                contentDescription = null
-            )
-            VolleyText.TitleLarge(
-                modifier = Modifier.rotate(VolleyDimens.ROTATION_8),
-                text = stringResource(R.string.invite_players),
-                color = VolleyColor.TextDark
-            )
-        }
-    }
-}
-
-@Composable
-private fun ShareButton(
-    modifier: Modifier,
-    onClick: () -> Unit
-) {
-    TransparentContainer(
-        modifier = modifier.height(VolleyDimens.DIMEN_180.dp),
-    ) {
-        Button(
-            modifier = modifier.height(VolleyDimens.DIMEN_180.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent,
-            ),
-            contentPadding = PaddingValues(
-                top = VolleyDimens.DIMEN_20.dp,
-                start = VolleyDimens.DIMEN_20.dp
-            ),
-            shape = RoundedCornerShape(VolleyDimens.DIMEN_32.dp),
-            onClick = onClick
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                Image(
-                    modifier = Modifier.align(Alignment.Center),
-                    painter = painterResource(R.drawable.image_share_link),
-                    contentDescription = null
-                )
-                VolleyText.TitleLarge(
-                    modifier = Modifier.rotate(VolleyDimens.ROTATION_8),
-                    text = stringResource(R.string.share_link),
-                    color = VolleyColor.White
-                )
-            }
-        }
-    }
-}
-
 @Preview(
     showBackground = true,
     showSystemUi = true,
     device = "spec:width=375dp,height=812dp"
 )
 @Composable
-private fun RatePlayersPreview() {
+private fun SuccessScreenPreview() {
     VolleybolleyTheme {
         Box(
             Modifier
