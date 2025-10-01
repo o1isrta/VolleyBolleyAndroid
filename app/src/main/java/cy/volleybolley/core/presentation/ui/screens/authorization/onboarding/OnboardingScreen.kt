4aff161a685bca058a43da66f59d87e46b708654
Evlenium <Evlenium@mail.ru>
2025-10-01 18:04:08 +0300

    Fix paddings

--- app/src/main/java/cy/volleybolley/core/presentation/ui/screens/authorization/onboarding/OnboardingScreen.kt
+++ app/src/main/java/cy/volleybolley/core/presentation/ui/screens/authorization/onboarding/OnboardingScreen.kt
@@ -63,8 +63,8 @@ fun OnboardingScreen(
             )
             LogoWithAppName(
                 modifier = Modifier
-                .padding(top = 80.dp)
-                .fillMaxWidth()
+                    .padding(top = 80.dp)
+                    .fillMaxWidth()
             )
             Spacer(modifier = Modifier.weight(1f))
             Box(
