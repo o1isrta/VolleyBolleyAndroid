f8991898385f154dc5b64eb348aef3edfee67dfc
ShevaVarya <shevchik.varvara@mail.ru>
2025-10-07 20:09:55 +0300

    47 - решение конфликта

--- app/src/main/java/cy/volleybolley/core/DiProvider.kt
+++ app/src/main/java/cy/volleybolley/core/DiProvider.kt
@@ -8,7 +8,6 @@ import cy.volleybolley.core.presentation.ui.screens.authorization.launch.di.laun
 import cy.volleybolley.core.presentation.ui.screens.authorization.registration.di.registrationModule
 import cy.volleybolley.courts.di.courtsModule
 import cy.volleybolley.games.di.gamesModule
-import cy.volleybolley.profile.di.profileModule
 import cy.volleybolley.players.di.playersModule
 import cy.volleybolley.profile.di.profileModule
 import cy.volleybolley.referencedata.di.referenceDataModule
