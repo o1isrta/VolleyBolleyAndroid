e771a51bbb369bf116d224e4518384680e543d76
Pavel_Davydov <david20506@yandex.ru>
2026-03-06 11:32:26 +0500

    Исправление замечаний reviewDog 3 попытка

--- app/src/main/java/cy/volleybolley/auth/phone/ui/PhoneInputBlock.kt
+++ app/src/main/java/cy/volleybolley/auth/phone/ui/PhoneInputBlock.kt
@@ -1,4 +1,4 @@
-package cy.volleybolley.auth.ui.phone
+package cy.volleybolley.auth.phone.ui
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
