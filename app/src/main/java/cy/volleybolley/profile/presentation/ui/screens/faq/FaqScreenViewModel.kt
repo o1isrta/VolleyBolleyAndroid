2a506e441822dfe5e475d11bed4fca1015b2566a
Dmitrii <richard.orlov13@gmail.com>
2026-03-26 14:40:59 +0300

    подключение к API

--- app/src/main/java/cy/volleybolley/profile/presentation/ui/screens/faq/FaqScreenViewModel.kt
+++ app/src/main/java/cy/volleybolley/profile/presentation/ui/screens/faq/FaqScreenViewModel.kt
@@ -1,23 +1,45 @@
 package cy.volleybolley.profile.presentation.ui.screens.faq
 
+import cy.volleybolley.core.domain.model.onFailure
+import cy.volleybolley.core.domain.model.onSuccess
 import cy.volleybolley.core.presentation.base.BaseViewModel
-import cy.volleybolley.core.presentation.ui.model.VolleyMocks
 import cy.volleybolley.core.presentation.ui.model.VolleyUiUtil
 import cy.volleybolley.profile.presentation.ui.screens.faq.FaqScreenEffect.NavigateFromFaqScreen
 import cy.volleybolley.profile.presentation.ui.screens.faq.FaqScreenEvent.OnBackFromFaqClick
+import cy.volleybolley.referencedata.domain.api.GetFaqUseCase
 import kotlinx.coroutines.flow.update
 
-class FaqScreenViewModel : BaseViewModel<FaqScreenState, FaqScreenEvent, FaqScreenEffect>(
+class FaqScreenViewModel(
+    private val getFaqUseCase: GetFaqUseCase
+) : BaseViewModel<FaqScreenState, FaqScreenEvent, FaqScreenEffect>(
     initialState = FaqScreenState()
 ) {
+
     init {
-        // Mock init
-        uiStateMutable.update { FaqScreenState(faqText = VolleyUiUtil.parseMarkdown(VolleyMocks.MOCK_FAQ)) }
+        loadFaq()
     }
 
     override fun obtainEvent(event: FaqScreenEvent) {
         when (event) {
             OnBackFromFaqClick -> sendUiEffect(NavigateFromFaqScreen(null))
+        }
+    }
+
+    private fun loadFaq() {
+        launchSafe(
+            getErrorLogMessage = { "Error loading FAQ: ${it.message}" }
+        ) {
+            getFaqUseCase.execute()
+                .onSuccess { faq ->
+                    val parsed = VolleyUiUtil.parseMarkdown(faq.faq)
+
+                    uiStateMutable.update {
+                        it.copy(faqText = parsed)
+                    }
+                }
+                .onFailure {
+                    println("FAQ ERROR: $it")
+                }
         }
     }
 }
